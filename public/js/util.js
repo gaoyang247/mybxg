ac6f6a282d5d6fec96f23d5d5b671f6ec137f271
gaoyang <gaoyang@163.con>
2017-09-05 20:11:56 +0800

    初步完成讲师管理

--- public/js/util.js
+++ public/js/util.js
@@ -5,12 +5,14 @@ define(['jquery'],function($){
 			$('.navs a[href="'+path+'"]').addClass('active');
 		},
 		qs : function(key){
-			var param = location.search.substring(1);
+			// qs 获取URL参数中指定的参数值
+			// param 是URL地址 ？号后面所有的参数（uname=lisi&age=12 参数的格式）
+			var param = location.search.substring(1);//删除参数的第一个问号
 			var result = null;
 			if(param){
-				var kvs =param.split('&');
+				var kvs =param.split('&');//将多个参数用&分割 kvs就是分割后的数组
 				$.each(kvs,function(i,item){
-					var kv = item.split('=');
+					var kv = item.split('=');//数组是有多个键值对组成，用 = 分割
 					if(key == kv[0]){
 						//找到对应的参数
 						result = kv[1];
